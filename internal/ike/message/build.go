5003296ef7e4815525782bce743b5cd210c0ab33
Ciro Macedo <ciromacedo@gmail.com>
2024-07-31 06:39:13 -0300

    add new features

--- internal/ike/message/build.go
+++ internal/ike/message/build.go
@@ -256,15 +256,16 @@ func BuildEAP5GANParameters() []byte {
 	log.Info("pZeroGuami - : ", vlZeroGuami)
 	log.Info("pUmGuami - : ", vlUmGuami)
 	log.Info("pDoisGuami - : ", vlDoisGuami)
-	
+
 	log.Info("Mcc and Mnc In Octets: %x\r", resu)
 
 	log.Info("Build GUAMI - Init ")
 	anParameter := make([]byte, 2)
 	guami := make([]byte, 6)
-	guami[0] = utils.ConvertToHexByte("2")  //0x02
-	guami[1] = utils.ConvertToHexByte("f8") //0xf8
-	guami[2] = utils.ConvertToHexByte("39") //0x39
+	guami[0] = utils.ConvertToHexByte(vlZeroGuami)  //0x02
+	guami[1] = utils.ConvertToHexByte(vlUmGuami) //0xf8
+	guami[2] = utils.ConvertToHexByte(vlDoisGuami) //0x39
+	
 	guami[3] = utils.ConvertToHexByte("ca") //0xca
 	guami[4] = utils.ConvertToHexByte("fe") //0xfe
 	guami[5] = utils.ConvertToHexByte("0")  //0x0
